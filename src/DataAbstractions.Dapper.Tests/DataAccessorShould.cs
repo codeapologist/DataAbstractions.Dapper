14fc24ff77f7adab2fadab5c8d13ca8cf12282e7
Andrew Sumido <codeapologist@gmail.com>
2020-10-10 23:32:14 -0500

    Added more tests around QueryAsync

--- src/DataAbstractions.Dapper.Tests/DataAccessorShould.cs
+++ src/DataAbstractions.Dapper.Tests/DataAccessorShould.cs
@@ -1,21 +1,82 @@
+using Dapper;
 using FluentAssertions;
 using System.Data.SqlClient;
+using System.Linq;
 using Xunit;
 
 namespace DataAbstractions.Dapper.Tests
 {
     public class DataAccessorShould
     {
-        private IDataAccessor _dataAccessor;
         public DataAccessorShould()
         {
-            _dataAccessor = new DataAccessor(new SqlConnection("Data Source=localhost;Database=TestDb;User ID=sa;Password=t3st#database"));
+        }
+
+        public IDataAccessor CreateDataAccessor()
+        {
+            return new DataAccessor(new SqlConnection("Data Source=localhost;Database=TestDb;User ID=sa;Password=t3st#database"));
+        }
+
+        [Fact]
+        public async void WorkWithQueryAsync()
+        {
+
+            using var dataAccessor = CreateDataAccessor();
+
+            dataAccessor.Open();
+
+            using var transaction = dataAccessor.BeginTransaction();
+
+            var result = await dataAccessor.QueryAsync("select * from Company where id = @Id", new { Id = 1 }, transaction, null, commandType: System.Data.CommandType.Text);
+            int? id = result.ToList().Single().Id;
+            string name = result.ToList().Single().Name;
+            id.Should().Be(1);
+            name.Should().Be("Mock Company");
+        }
+
+        [Fact]
+        public async void WorkWithQueryAsyncOverload1()
+        {
+
+            using var dataAccessor = CreateDataAccessor();
+
+            dataAccessor.Open();
+
+            using var transaction = dataAccessor.BeginTransaction();
+
+            var definition = new CommandDefinition("select * from Company where id = @Id", new { Id = 1 }, transaction, null, commandType: System.Data.CommandType.Text, CommandFlags.Buffered, default);
+
+            var result = await dataAccessor.QueryAsync(definition);
+            int? id = result.ToList().Single().Id;
+            string name = result.ToList().Single().Name;
+            id.Should().Be(1);
+            name.Should().Be("Mock Company");
+        }
+
+
+
+        [Fact]
+        public async void WorkWithQueryAsyncOfT()
+        {
+
+            using var dataAccessor = CreateDataAccessor();
+
+            dataAccessor.Open();
+
+            using var transaction = dataAccessor.BeginTransaction();
+
+            var result = await dataAccessor.QueryAsync<Company>("select * from Company where id = @Id", new { Id = 1 }, transaction, null, commandType: System.Data.CommandType.Text);
+            var expected = new Company { Id = 1, Name = "Mock Company" };
+            result.Should().BeEquivalentTo(expected);
+
         }
 
         [Fact]
         public async void WorkWithQuerySingleAsync()
         {
-            var result = await _dataAccessor.QuerySingleAsync<Company>("select * from Company where id = 1", commandType: System.Data.CommandType.Text);
+
+            using var dataAccessor = CreateDataAccessor();
+            var result = await dataAccessor.QuerySingleAsync<Company>("select * from Company where id = 1", commandType: System.Data.CommandType.Text);
             var expected = new Company { Id = 1, Name = "Mock Company" };
             result.Should().BeEquivalentTo(expected);
         }
@@ -23,8 +84,9 @@ namespace DataAbstractions.Dapper.Tests
         [Fact]
         public async void WorkWithExecuteReaderAsync()
         {
-            var reader = await _dataAccessor.ExecuteReaderAsync("select * from Company where id = 1");
-            var dataReaderAccessor = _dataAccessor.GetDataReaderAbstraction(reader);
+            using var dataAccessor = CreateDataAccessor();
+            var reader = await dataAccessor.ExecuteReaderAsync("select * from Company where id = 1");
+            var dataReaderAccessor = dataAccessor.GetDataReaderAbstraction(reader);
             var result = dataReaderAccessor.Parse<Company>();
             var expected = new Company { Id = 1, Name = "Mock Company" };
             result.Should().BeEquivalentTo(expected);
